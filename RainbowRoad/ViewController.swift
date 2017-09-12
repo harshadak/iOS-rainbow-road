92821625ef72228c6a0777c83275a499c618c977
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2017-09-11 18:18:53 -0700

    Finished the assignment. Added background colors to individual cells in the UITableView.

--- RainbowRoad/ViewController.swift
+++ RainbowRoad/ViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    var colors = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.purple]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +24,18 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    // TableView methods
+    //     How many cells are we going to need?
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // return an integer that indicates how many rows (cells) to draw
+        return colors.count
+    }
+    
+    // How should I create each cell?
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get the UITableViewCell and create/populate it with data then return it
+        let cell = tableView.dequeueReusableCell(withIdentifier: "rainbow", for: indexPath)
+        cell.backgroundColor = colors[indexPath.row]
+        return cell
+    }
 }
-
